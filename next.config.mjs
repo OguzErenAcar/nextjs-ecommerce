94797454902da1ac1b95de2b9d2a45c43af6c8b2
oguz <oguz@oguz-MacBook-Pro.local>
2026-06-29 23:52:41 +0300

    auto: 2026-06-29 23:52

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
-// next.config.mjs
-import transpileModules from 'next-transpile-modules';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: undefined,
   images: { unoptimized: true },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias, 
-    };
-    return config;
-  },
+  transpilePackages: [
+    '@mui/material',
+    '@mui/system',
+    '@mui/icons-material',
+  ],
 };
 
-export default transpileModules([
-  '@mui/material',
-  '@mui/system',
-  '@mui/icons-material',
-])(nextConfig);
+export default nextConfig;
